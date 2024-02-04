3a1f0e90ea011f16dd9782e6e546d707c7a0d7a1
Bruno Fregoso Frias <bruno88756@gmail.com>
2024-02-03 21:00:03 -0500

    Need to fetch movie from tMD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import Papa from 'papaparse';
+import { useState } from "react";
+import './App.css'
 
 function App() {
+  const [parsedData, setParsedData] = useState([]);
+  const [values, setValues] = useState([]);
+
+  
+  
+  
+  
+  const changeHandler = (event) => {
+    Papa.parse(event.target.files[0],{
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results){
+        const valuesArray = []
+
+        results.data.map((d) => {
+          valuesArray.push(Object.values(d));
+        });
+
+        const sortedValues = valuesArray.sort((a, b) => {
+          const dateA = new Date(a[0]);
+          const dateB = new Date(b[0]);
+          return dateB - dateA;
+        });
+
+        setParsedData(results.data);
+        setValues(sortedValues);
+        console.log(valuesArray)
+      },
+    });
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="side-bar">
+        <h1 className='side-item'>Letterboxd topsters!</h1>
+        <input
+        type="file"
+        name="file"
+        accept=".csv"
+        style={{ display: "block" }}
+        className='side-item'
+        onChange={changeHandler}
+      />
+      </div>
+      <div className="middle">
+        <div className="collage-grid-container">
+          <div className="collage-grid">
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
